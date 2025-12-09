08e87c3e84c4920572eda3d47443a6acd6f2b302
rahelwilliam <rahel.william@gmail.com>
2025-12-09 14:28:21 -0300

    adicionado script

--- header.php
+++ header.php
@@ -19,9 +19,22 @@
     <meta property="og:image" content="<?php echo image('share.jpg'); ?>">
 
     <?php wp_head(); ?>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5DGRL8CJ');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body <?php (is_page('modelo-2')) ? body_class('body-wider body-boxed body-grad') : body_class('body-wider'); ?>>
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DGRL8CJ"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     
     <div id="app">
 
